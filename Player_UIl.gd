6e260f7c0235a7fc68529d4edb14cf9ccf3ed2e3
ANinnyWoozle <wuzhaotree@gmail.com>
2018-12-04 10:46:51 -0800

    Added Power UP management to UI scripts Image update for power up UI psuedocode added to Player_UI.tscn

--- Player_UIl.gd
+++ Player_UIl.gd
@@ -17,6 +17,13 @@ func update_track_stats(update_list):
 	lap_label.update_lap(2)
 	place_label.update_place(1)
 	
+func update_items_panel(poweruparray):
+	$"Items Panel/Main Item".update_image(poweruparray[0])
+	$"Items Panel/Item2".update_image(poweruparray[1])
+	$"Items Panel/Item3".update_image(poweruparray[2])
+	$"Items Panel/Item4".update_image(poweruparray[3])
+	$"Items Panel/Item5".update_image(poweruparray[4])
+	$"Items Panel/Item6".update_image(poweruparray[5])
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
