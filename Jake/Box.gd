21ca410e9f75cea4461a6f2725300f66712a4aab
ANinnyWoozle <wuzhaotree@gmail.com>
2018-12-04 16:35:50 -0800

    Power Up Pickups from collision with Box

--- Jake/Box.gd
+++ Jake/Box.gd
@@ -18,5 +18,5 @@ func _on_Box_body_entered(body):
 	print(body.name)
 	if body is KinematicBody:# && body.name.begins_with("Player") == true:
 		print(body.name)
-		#body.PowerUpManager.AddPowerUp()
+		body.get_child(1).AddPowerUp()
 		get_parent().process_pickup(self)
